d4150524afa18e44f38c597faf5ebef74902a7e6
bytepull <luca.giommoni91@gmail.com>
2025-05-08 22:39:01 +0200

    refactor(SideMenu): improve layout and structure for better readability
    Restructure the SideMenu component to enhance clarity and organization. Adjust padding, add sections for "Management" and "Help", and improve button alignment. These changes aim to make the UI more intuitive and maintainable.

--- src/app/sections/SideMenu.tsx
+++ src/app/sections/SideMenu.tsx
@@ -21,7 +21,7 @@ function SideMenu({ actions, states }: SideMenuProps): React.JSX.Element {
   const [offset, setOffset] = useState(0);
   const [changeOffset, setChangeOffset] = useState(0);
 
-  const menuButtonsCSSClass = "flex items-center w-full px-8 py-4 text-sm";
+  const menuButtonsCSSClass = "flex items-center w-full px-2 py-4 text-sm";
 
   return (
     <div
@@ -43,57 +43,69 @@ function SideMenu({ actions, states }: SideMenuProps): React.JSX.Element {
       aria-orientation="vertical"
       aria-labelledby="menu-button"
       tabIndex={-1}
-      className={`fixed top-0 letf-0 z-50 flex h-screen flex-col justify-between border-e border-gray-100 dark:border-gray-900 bg-white dark:bg-gray-800 transition-transform duration-1000 transform ${
+      className={`fixed top-0 letf-0 z-50 flex flex-col h-screen w-screen border-e border-gray-100 dark:border-gray-900 bg-white dark:bg-gray-800 transition-transform duration-1000 transform ${
         states.stateOfMenu ? "translate-x-0" : "-translate-x-full"
       }`}
     >
-      <div className="py-6 space-y-8">
-        <div className="flex space-x-4 text-xl p-4 m-4 border-b-2 border-gray-200 dark:border-gray-50/40">
-          {/* TODO: Logo */}
-          <h1>Loyalty Wallet</h1>
-          <button>
-            <IoCloseOutline
-              className="absolute top-0 left-0 h-6 w-6 m-4 cursor-pointer"
-              onClick={actions.closeMenu}
-            />
-          </button>
-        </div>
+      <div className="w-full p-4">
+        <button className="p-4">
+          <IoCloseOutline
+            className="h-8 w-8 cursor-pointer"
+            onClick={actions.closeMenu}
+          />
+        </button>
 
-        <div className="">
-          <button
-            type="button"
-            role="menuitem"
-            className={menuButtonsCSSClass}
-            onClick={actions.openInfoSection}
-          >
-            <CiCircleInfo className="h-6 w-6 mr-4" />
-            <p>Info</p>
-          </button>
-          <button
-            type="button"
-            className={menuButtonsCSSClass}
-            onClick={actions.handleImport}
-          >
-            <CiImport className="h-6 w-6 mr-4" />
-            <p>Import</p>
-          </button>
-          <button
-            type="button"
-            role="menuitem"
-            className={menuButtonsCSSClass}
-            onClick={actions.handleExport}
-          >
-            <CiExport className="h-6 w-6 mr-4" />
-            <p>Export</p>
-          </button>
-          <button
-            type="button"
-            className={menuButtonsCSSClass}
-            onClick={actions.handleDeleteAllCards}
-          >
-            <CiTrash className="h-6 w-6 mr-4" />
-            <p>Delete All Cards</p>
-          </button>
+        <div className="space-y-8">
+          <div className="text-xl text-center font-bold pb-4 pt-2 mx-4 border-b-2 border-gray-200 dark:border-gray-50/40">
+            {/* TODO: Logo */}
+            <h1>Loyalty Wallet</h1>
+          </div>
+
+          <div className="px-4">
+            <div className="text-lg font-bold mb-4">Management</div>
+            <div className="divide-y divide-gray-200">
+              <button
+                type="button"
+                className={menuButtonsCSSClass}
+                onClick={actions.handleImport}
+              >
+                <CiImport className="h-6 w-6 mr-4" />
+                <p>Import Cards</p>
+              </button>
+              <button
+                type="button"
+                role="menuitem"
+                className={menuButtonsCSSClass}
+                onClick={actions.handleExport}
+              >
+                <CiExport className="h-6 w-6 mr-4" />
+                <p>Export Cards</p>
+              </button>
+              <button
+                type="button"
+                className={menuButtonsCSSClass}
+                onClick={actions.handleDeleteAllCards}
+              >
+                <CiTrash className="h-6 w-6 mr-4" />
+                <p>Delete All Cards</p>
+              </button>
+            </div>
+          </div>
+
+          <div className="px-4">
+            <div className="text-lg font-bold mb-4">Help</div>
+            <div className="divide-y divide-gray-200">
+              <button
+                type="button"
+                role="menuitem"
+                className={menuButtonsCSSClass}
+                onClick={actions.openInfoSection}
+              >
+                <CiCircleInfo className="h-6 w-6 mr-4" />
+                <p>Info</p>
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
